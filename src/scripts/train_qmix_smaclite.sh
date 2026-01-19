fea6d19fb05ffb64f934cb7fab53dc8239f5247c
giangbang <banggiangle2015@gmail.com>
2026-01-19 09:47:30 +0700

    update sh script

--- src/scripts/train_qmix_smaclite.sh
+++ src/scripts/train_qmix_smaclite.sh
@@ -2,9 +2,9 @@
 algo="qmix"
 seed_max=2
 scenarios=( "MMM" "MMM2" "corridor" "bane_vs_bane" "25m_vs_30m" "3s5z_vs_3s6z")
-total_times=( 1_000_000 2_000_000 3_000_000 2_000_000 2_000_000 5_000_000 )
-scenarios=( "25m_vs_30m" )
-total_times=( 2_000_000 )
+total_times=( 1_000_000 2_000_000 5_000_000 2_000_000 2_000_000 5_000_000 )
+scenarios=( "corridor" )
+total_times=( 5_000_000 )
 
 
 n_exp=${#scenarios[@]}
@@ -15,7 +15,7 @@ for ((i=0; i<n_exp; i++)); do
         scenario="${scenarios[$i]}"
         total_ts="${total_times[$i]}"
         python src/main.py --config=${algo} --env-config=smaclite with env_args.time_limit=150 env="smaclite" \
-                env_args.map_name="custom-smaclite/${scenario}" \
-                t_max=${total_ts}  batch_size=64 batch_size_run=4 buffer_size=2500  # for bane and 25m due to OOM
+                env_args.map_name="custom-smaclite/${scenario}"  t_max=${total_ts} #\
+                # batch_size=64 batch_size_run=4 buffer_size=2500  # for bane and 25m due to OOM
     done
 done
